d4c1ec9f73d293d6c0c9e2b6ca9cc7e0166bb938
perla ravi kumar <42589484+perla0207@users.noreply.github.com>
2022-05-28 21:36:27 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
 streamlit.title('Welcome to ColeHaaan')
+streamlit.header('Colehaan Brand New Items')
+streamlit.title('Shoes')
+streamlit.title('Bags')
+streamlit.title('Clothes')
+streamlit.title('Wallets')
