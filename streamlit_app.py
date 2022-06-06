142a6fee3ba4330a92e2843bef0aa8581070310f
perla ravi kumar <42589484+perla0207@users.noreply.github.com>
2022-06-06 19:45:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,16 @@ my_data_row=my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
 
+import snowflake.connector
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("select * from fruit_load_list")
+my_data_row = my_cur.fetchone()
+streamlit.text("My fruitLoad List Contains")
+streamlit.text(my_data_row)
 
+
+/*
 import requests
 fruity_response=requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
@@ -45,3 +54,4 @@ fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
 #Output it the screen as a table
 
 streamlit.dataframe(fruityvice_normalized)
+*/
